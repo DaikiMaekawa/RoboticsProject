f47449443c4494e7ca87f7a06d9c42ad6db31a5d
maekawadaiki <maekawadaiki@ubuntu.ubuntu-domain>
2013-03-16 00:43:41 +0900

    Add a timeout to the recognition

---
+++ Atlas/Timer.cpp
@@ -0,0 +1,37 @@
+#include "Timer.h"
+#include <iostream>
+
+namespace atlas{
+
+    void Timer::roop(unsigned long cycle, boost::function<void()> callback, unsigned int callbackNum){
+        unsigned int callbackCount = 0;
+
+        while(true){
+            if(m_endFlag) break;
+            if(callbackNum != 0 && callbackCount >= callbackNum) break;
+            
+            boost::this_thread::sleep(boost::posix_time::seconds(cycle));
+            callback();
+            if(callbackNum != 0) callbackCount++;
+        }
+    }
+
+    void Timer::singleShot(unsigned long sec, boost::function<void()> callback){
+        m_endFlag = false;
+        boost::thread thread(boost::bind(&Timer::roop, this, sec, callback, 1));
+    }
+
+    void Timer::start(unsigned long cycle, boost::function<void()> callback){
+        m_endFlag = false;
+        boost::thread thread(boost::bind(&Timer::roop, this, cycle, callback, 0));
+    }
+
+    void Timer::stop(){
+        m_endFlag = true;
+    }
+
+    Timer::~Timer(){
+        stop();
+    }
+
+} //namespace atlas
---
+++ Atlas/Timer.h
@@ -0,0 +1,31 @@
+/*******************************************************************
+ * Copyright (c) 2011 Daiki Maekawa
+ *
+ * @file Timer.h
+ * @brief Used as a simple timer
+ * @author Daiki Maekawa
+ * @date 2011-10-19
+ *******************************************************************/
+
+#pragma once
+#include <boost/function.hpp>
+#include <boost/functional.hpp>
+#include <boost/thread.hpp>
+
+namespace atlas{
+
+    class Timer{
+        boost::mutex m_mutex;
+        boost::condition_variable_any m_exitRequest;
+        volatile bool m_endFlag;
+        void roop(unsigned long cycle, boost::function<void()> callback, unsigned int callbackNum = 0);
+    public:
+        Timer() : m_endFlag(false) { }
+        ~Timer();
+        void singleShot(unsigned long sec, boost::function<void()> callback);
+        void start(unsigned long cycle, boost::function<void()>); //cycle[sec]
+        void stop();
+    };
+
+} //namespace atlas
+
