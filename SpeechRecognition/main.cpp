940a8dc957fef462dbe0372fc1f096651195706e
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2013-03-16 16:22:26 +0900

    Add a character code conversion function using icu

--- SpeechRecognition/main.cpp
+++ SpeechRecognition/main.cpp
@@ -1,18 +1,15 @@
 #include "SpeechRecognition.h"
 #include <iostream>
 #include <string>
-#include <boost/thread.hpp>
 
 using namespace nui;
 using namespace std;
 
 int main(int argc, char *argv[]){
     SpeechRecognition recog(argc,argv);
-    
     cout << "recog.isValid() = " << recog.isValid() << endl;
     
-    cout <<  recog.startRecognition(5) << endl;;
-    cout << "startRecognition : end" << endl;
+    cout << "startRecognition = " << recog.startRecognition(5) << endl;
 
     vector<string> results = recog.resultsString();
     for(int i=0; i < results.size(); i++){
