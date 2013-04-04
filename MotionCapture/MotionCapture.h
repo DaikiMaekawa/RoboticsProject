6aba10d5f1fa4321aa49a199428303efb4e57b00
DaikiMaekawa <daikimaekawa@daikimaekawa-VirtualBox.(none)>
2013-04-05 01:15:53 +0900

    Add: motion capture tool

---
+++ MotionCapture/MotionCapture.h
@@ -0,0 +1,22 @@
+#ifndef MOTIONCAPTURE_H
+#define MOTIONCAPTURE_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MotionCapture;
+}
+
+class MotionCapture : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MotionCapture(QWidget *parent = 0);
+    ~MotionCapture();
+    
+private:
+    Ui::MotionCapture *ui;
+};
+
+#endif // MOTIONCAPTURE_H
---
+++ MotionCapture/main.cpp
@@ -0,0 +1,11 @@
+#include <QtGui/QApplication>
+#include "MotionCapture.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MotionCapture w;
+    w.show();
+    
+    return a.exec();
+}
